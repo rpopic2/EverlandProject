d66d21240064fb14e210988734899cadf969f584
sua <ksa3067@gmail.com>
2022-04-20 13:55:10 +0900

    1. 캐릭터 애니메이션 변경 가능하도록 설정 2. 이어폰 권장 화면 구현 3. 엔딩 크레딧에 종료 버튼, 종료 페이지 추가

--- Assets/02_Scripts/PlayerSetting.cs
+++ Assets/02_Scripts/PlayerSetting.cs
@@ -19,16 +19,58 @@ public class PlayerSetting : MonoBehaviour
     public GameObject username1;
     public GameObject username2;
 
+    public GameObject ShowPanel;
+    public GameObject ClosePanel;
+
+
+    float time = 0f;
+    float fadeTime = 2f;
+
+
     void Start()
     {
         username1.GetComponent<Text>().text = playerName;
         username2.GetComponent<Text>().text = playerName;
-
+        //StartCoroutine("FadeOut", 3);
+        //Invoke("HeadPhonePlz", 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void HeadPhonePlz()
+    {
+        GameObject.Find("HeadPhonePlz").SetActive(false);
+    }
+
+    public IEnumerator FadeOut(float time)
+    {
+        Color color = GameObject.Find("HeadPhonePlz").GetComponent<Image>().color;
+        while (color.a < 1f)
+        {
+            color.a += Time.deltaTime / time;
+            GameObject.Find("HeadPhonePlz").GetComponent<Image>().color = color;
+            yield return null;
+        }
+    }
+
+    public void ShowCloseBtn()
+    {
+        ClosePanel.gameObject.SetActive(false);
+        ShowPanel.gameObject.SetActive(true);
+    }
+
+    public void BackBtn()
+    {
+        ShowPanel.gameObject.SetActive(false);
+        ClosePanel.gameObject.SetActive(true);
+    }
+
+    public void CloseBtn()
+    {
+        Application.Quit();
     }
 }
